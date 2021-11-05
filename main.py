6377306aaafeadd42a3e28defe26f3a6688d282f
Robert Klajko <klajkor@gmail.com>
2021-11-05 22:38:43 +0100

    Converting and sending sensor values to ThingSpeak implemented

--- main.py
+++ main.py
@@ -4,26 +4,36 @@
 Converting MQTT messages and sending them to ThingSpeak
 """
 
-import random
-
 import secrets
-
+import random
+import json
+import time
+import thingspeak
 from paho.mqtt import client as mqtt_client
-
 
 
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 
+# How to map JSON topics of MQTT messages to ThingSpeak fields
+FIELD_MAPPER_LIST = [
+    {"json_topic":"SI7021", "json_subtopic":"Temperature", "ts_field":"field1"},
+    {"json_topic":"SI7021", "json_subtopic":"Humidity", "ts_field":"field2"},
+    {"json_topic":"BH1750", "json_subtopic":"LightLevel", "ts_field":"field3"},
+    {"json_topic":"PIR1", "json_subtopic":"StatusNum", "ts_field":"field4"},
+    {"json_topic":"BH1750", "json_subtopic":"LightAlarmStatusNum", "ts_field":"field5"}
+]
+
+
 def connect_mqtt() -> mqtt_client:
     """ Connect to MQTT broker """
 
-    def on_connect(client, userdata, flags, rc):
+    def on_connect(client, userdata, flags, return_code):
         """ on_connect callback """
-        if rc == 0:
+        if return_code == 0:
             print("Connected to MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            print("Failed to connect, return code %d\n", return_code)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set(secrets.MQTT_USERNAME, secrets.MQTT_PASSWORD)
@@ -32,31 +42,50 @@ def connect_mqtt() -> mqtt_client:
     return client
 
 
-def subscribe_sensor(client: mqtt_client):
-    """ Subscribe to MQTT topic """
-    def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-    client.subscribe(secrets.MQTT_TOPIC_SENSOR)
-    client.on_message = on_message
+def subscribe_tele_root(client_sensor: mqtt_client):
+    """ Subscribe to 'tele root' and additional MQTT sub-topics """
+    client_sensor.subscribe(secrets.MQTT_TOPIC_TELE_ROOT)
+    client_sensor.on_message = on_message_tele_root
+    client_sensor.message_callback_add(secrets.MQTT_TOPIC_SENSOR, on_message_tele_sensor)
 
 
-def subscribe_state(client: mqtt_client):
-    """ Subscribe to MQTT topic """
-    def on_message(client, userdata, msg):
-        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-
-    client.subscribe(secrets.MQTT_TOPIC_STATE)
-    client.on_message = on_message
+def on_message_tele_root(client_sensor, userdata, msg):
+    """ On message callback for general messages not handled by topic specific callbacks
+    Currently do nothing """
 
 
-def run():
+def on_message_tele_sensor(client_sensor, userdata, msg):
+    """ Decoding incoming MQTT message and sending to ThingSpeak """
+    decoded_message = msg.payload.decode("utf-8")
+    json_message = json.loads(decoded_message)
+    print(f"SENSOR Received from `{msg.topic}` topic")
+    ts_payload = {}
+
+    for field_map in FIELD_MAPPER_LIST:
+        json_topic = field_map['json_topic']
+        if json_topic in json_message:
+            topic_json_message = json_message[json_topic]
+            json_subtopic = field_map['json_subtopic']
+            if json_subtopic in topic_json_message:
+                subtopic_value = topic_json_message[json_subtopic]
+                print(f"'{json_subtopic}': '{subtopic_value}'")
+                ts_field = field_map['ts_field']
+                ts_payload[ts_field] = subtopic_value
+
+    if len(ts_payload) > 0:
+        print(f"TS payload: '{ts_payload}'")
+        time.sleep(15)
+        THINGSPEAK_CHANNEL.update(ts_payload)
+
+
+def run(ts_channel_run: thingspeak.Channel):
     """ Run MQTT connect """
     client = connect_mqtt()
-    subscribe_sensor(client)
-    subscribe_state(client)
+    subscribe_tele_root(client)
     client.loop_forever()
 
 
 if __name__ == '__main__':
-    run()
+    THINGSPEAK_CHANNEL = thingspeak.Channel(id = secrets.THINGSPEAK_CHANNEL_ID,
+        api_key = secrets.THINGSPEAK_API_KEY)
+    run(THINGSPEAK_CHANNEL)
--- secrets_example.py
+++ secrets_example.py
@@ -5,7 +5,10 @@ Secrets example file:
 
 MQTT_BROKER = 'your.mqtt.broker'
 MQTT_PORT = 1883
+MQTT_TOPIC_TELE_ROOT = "tele/#"
 MQTT_TOPIC_SENSOR = "tele/topic/SENSOR"
-MQTT_TOPIC_STATE = "tele/topic/STATE"
 MQTT_USERNAME = 'username'
 MQTT_PASSWORD = 'SuperSecretPassword'
+
+THINGSPEAK_CHANNEL_ID = 123456789
+THINGSPEAK_API_KEY = 'Your_API_Key'
