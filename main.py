a6d20ea417681b7edfdfe654baeb9aa2e7d4025b
Robert Klajko <klajkor@gmail.com>
2021-11-07 19:12:04 +0100

    Console log messages refactored

--- main.py
+++ main.py
@@ -31,23 +31,20 @@ def connect_mqtt() -> mqtt_client:
 
     def on_connect(client, userdata, flags, return_code):
         """ on_connect callback """
-        time_now = datetime.datetime.now()
-        time_now_str = time_now.strftime("%Y-%m-%d %H:%M:%S")
         if return_code == 0:
-            print(f"{time_now_str} Connected to MQTT Broker, started to subscribe.")
+            console_log("Connected to MQTT Broker, started to subscribe.")
             subscribe_tele_root(client)
         else:
-            print(f"{time_now_str} >>FAILED to connect, return code %d\n", return_code)
+            console_log(f">>FAILED to connect, return code:{return_code}\n")
 
     def on_disconnect(client, userdata, return_code):
         """ on_disconnect callback """
-        time_now = datetime.datetime.now()
-        time_now_str = time_now.strftime("%Y-%m-%d %H:%M:%S")
         if return_code != 0:
-            print(f"{time_now_str} >>UNEXPECTED disconnection.")
+            console_log(">> UNEXPECTED disconnection, return code:{return_code}\n")
         else:
-            print(f"{time_now_str} >>Normal disconnection.")
+            console_log(">> Normal disconnection.")
 
+    console_log(f"Starting MQTT connection to {secrets.MQTT_BROKER}")
     client = mqtt_client.Client(client_id)
     client.username_pw_set(secrets.MQTT_USERNAME, secrets.MQTT_PASSWORD)
     client.on_connect = on_connect
@@ -59,14 +56,12 @@ def connect_mqtt() -> mqtt_client:
 def subscribe_tele_root(client_sensor: mqtt_client):
     """ Subscribe to 'tele root' and additional MQTT sub-topics """
     (result, mid) = client_sensor.subscribe(secrets.MQTT_TOPIC_TELE_ROOT)
-    time_now = datetime.datetime.now()
-    time_now_str = time_now.strftime("%Y-%m-%d %H:%M:%S")
     if result == mqtt_client.MQTT_ERR_SUCCESS:
-        print(f"{time_now_str} Subscribed to: '{secrets.MQTT_TOPIC_TELE_ROOT}'")
+        console_log(f"Subscribed to: '{secrets.MQTT_TOPIC_TELE_ROOT}'")
         client_sensor.on_message = on_message_tele_root
         client_sensor.message_callback_add(secrets.MQTT_TOPIC_SENSOR, on_message_tele_sensor)
     else:
-        print(f"{time_now_str} >>FAILED to subscribe to: '{secrets.MQTT_TOPIC_TELE_ROOT}'")
+        console_log(f">> FAILED to subscribe to: '{secrets.MQTT_TOPIC_TELE_ROOT}'")
 
 
 def on_message_tele_root(client_sensor, userdata, msg):
@@ -87,15 +82,13 @@ def on_message_tele_sensor(client_sensor, userdata, msg):
             topic_json_message = json_message[json_topic]
             json_subtopic = field_map['json_subtopic']
             if json_subtopic in topic_json_message:
-                time_now = datetime.datetime.now()
-                time_now_str = time_now.strftime("%Y-%m-%d %H:%M:%S")
                 subtopic_value = topic_json_message[json_subtopic]
-                print(f"{time_now_str} '{json_subtopic}': '{subtopic_value}'")
+                console_log(f"'{json_subtopic}': '{subtopic_value}'")
                 ts_field = field_map['ts_field']
                 ts_payload[ts_field] = subtopic_value
 
     if len(ts_payload) > 0:
-        print(f"{time_now_str} TS payload: '{ts_payload}'")
+        console_log(f"TS payload: '{ts_payload}'")
         time.sleep(15)
         THINGSPEAK_CHANNEL.update(ts_payload)
 
@@ -104,6 +97,12 @@ def run(ts_channel_run: thingspeak.Channel):
     """ Run MQTT connect """
     client = connect_mqtt()
     client.loop_forever()
+
+
+def console_log(log_message):
+    """ Logging to console with timestamp """
+    time_now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"{time_now_str} {log_message}")
 
 
 if __name__ == '__main__':
